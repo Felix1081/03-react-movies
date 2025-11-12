ad06e5aec30be38003748fc3cbc78e2368feb2e2
Yevhenii Shashurkin <lifeinkiev@i.ua>
2025-11-12 13:30:47 +0200

    start working on movieServises

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import SearchBar from "../SearchBar/SearchBar";
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSearchSubmit} />
+    </div>
+  );
+}
---
+++ src/components/services/movieService.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import type { Movie } from "../../types/movie";
+
+const TMBD_BASE_URL = "https://api.themoviedb.org/3";
+const TMBD_TOKEN = import.meta.env.VITE_TMDB_TOKEN as string;
+
+interface FetchMoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface FetchMoviesParams {
+  query: string;
+  include_adult?: boolean;
+  language?: string;
+  primary_release_year?: string;
+  page?: number;
+  region?: string;
+  year?: string;
+}
+
+export async function fetchMovies(params: FetchMoviesParams): Promise<Movie[]> {
+  const url = `${TMBD_BASE_URL}/search/movie`;
+  const response = await axios.get<FetchMoviesResponse>(url, {
+    params,
+    headers: { Authorization: `Bearer ${TMBD_TOKEN}` },
+  });
+  return response.data.results;
+}
